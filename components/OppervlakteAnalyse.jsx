f2a029b0a56e8265fec8a48eb396b017a260044c
Thierry1996NL <1996thierry@live.nl>
2026-05-17 16:42:14 +0200

    Update OppervlakteAnalyse.jsx

--- components/OppervlakteAnalyse.jsx
+++ components/OppervlakteAnalyse.jsx
@@ -906,6 +906,7 @@ export default function OppervlakteAnalyse({ project, onAnalyseOpgeslagen }) {
                     </a>
                   </div>
                 )}
+              </div>
             );
           })}
         </div>
